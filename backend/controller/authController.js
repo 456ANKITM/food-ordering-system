6fbf22a3aa149f89c99d8a83b3330e62dc2edc16
Ankit Mishra <am9491691@gmail.com>
2026-06-16 19:27:38 +0545

    cookie fixed

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -126,8 +126,8 @@ export const login = async (req, res) => {
     // ✅ Set httpOnly cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -194,7 +194,7 @@ export const loginAdmin = async (req, res) => {
       res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
