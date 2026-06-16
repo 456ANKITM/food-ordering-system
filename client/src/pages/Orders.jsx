a599ef4fae37114c74d178fc5e33643333d70545
Ankit Mishra <am9491691@gmail.com>
2026-06-16 13:26:34 +0545

    test

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -58,7 +58,7 @@ const StripeCheckout = ({ clientSecret, onSuccess }) => {
     const result = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url:`${window.location.origin}/payment-result`,
+        return_url:`${window.location.origin}/payment-resultses`,
       },
     });
     setProcessing(false);
